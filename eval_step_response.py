f007d07d443e4605cd8a81481e34e1fd31bc4ed5
linzhanguca <lzhang12@uca.edu>
2025-11-04 10:39:30 -0600

    update script

--- eval_step_response.py
+++ eval_step_response.py
@@ -2,37 +2,42 @@ from time import sleep
 from wheel_controller import WheelController
 from machine import Pin
 
-# SETUP
-velocity_controller = WheelController(
-    driver_ids=(15, 13, 14),
-    encoder_ids=(18, 19),
-)
-# START TUNING PID GAINS below
-velocity_controller.k_p = None
-velocity_controller.k_i = None
-velocity_controller.k_d = None
-# END TUNING
 
+# START TUNING PID GAINS below
+REF_VEL = 0.4
+K_P = 1.2
+K_I = 0.0
+K_D = 1.5
+
+# SETUP
+pid_controller = WheelController(
+    driver_ids=(15, 13, 14),
+    encoder_ids=(11, 10),
+)
+pid_controller.k_p = K_P
+pid_controller.k_i = K_I
+pid_controller.k_d = K_D
 STBY = Pin(12, Pin.OUT)
 STBY.on()
+meas_vels = []
+
 
 # LOOP
-
-for i in range(200):
-    if i == 50:  # step up @ t=0.5 s
-        velocity_controller.set_wheel_velocity(0.4)
-    elif i == 140:  # step down @ t=1.25 s
-        velocity_controller.set_wheel_velocity(0.0)
+for i in range(100):
+    if i == 24:  # step up @ t=0.5 s
+        pid_controller.set_wheel_velocity(ref_vel)
+    elif i == 74:  # step down @ t=1.5 s
+        pid_controller.set_wheel_velocity(0.0)
     print(
-        f"Reference velocity={velocity_controller.ref_lin_vel} m/s, Measured velocity={velocity_controller.meas_lin_vel} m/s"
+        f"Reference velocity={pid_controller.ref_lin_vel} m/s, Measured velocity={pid_controller.meas_lin_vel} m/s"
     )
     sleep(0.02)
 # Terminate
-velocity_controller.set_wheel_velocity(0.0)
+pid_controller.set_wheel_velocity(0.0)
 sleep(0.5)
 STBY.off()
 
 ### UNCOMMENT FOLLOWING 3 LINES WHEN SATISFIED WITH PID GAINS ###
-# with open(f'data-{K_P}_{K_I}_{K_D}.csv', 'w') as file:
+# with open(f'ref{REF_VEL}-{K_P}_{K_I}_{K_D}.csv', 'w') as file:
 #     for item in data:
 #         file.write(f'{item[0]},{item[1]}\n')
